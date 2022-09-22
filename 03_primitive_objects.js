44cd030a865d03f413f2b0d6b6de76c44739e2fd
Manikanta M <59594162+manikanta3566@users.noreply.github.com>
2022-09-22 22:25:37 +0530

    Create 03_primitive_objects.js

---
+++ 03_primitive_objects.js
@@ -0,0 +1,17 @@
+//primitive data type
+let a=null;
+let b=10;
+let c=true;
+let d=BigInt("22233333");
+let e="name";
+let f=undefined;
+let g=Symbol("hii hello");
+console.log(a,b,c,d,e,f,g);
+
+//objects
+const student={
+  "name":"abc",
+  "toatlMarks":650,
+  "active":true
+}
+console.log(student.name);
