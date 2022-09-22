35502ed6198a075c079732a8c90849cd8faab600
Manikanta M <59594162+manikanta3566@users.noreply.github.com>
2022-09-22 22:43:23 +0530

    Create 04_practice.js

---
+++ 04_practice.js
@@ -0,0 +1,14 @@
+//create a variable type string and add number to it
+let a="abc";
+a=a+20;
+console.log(a);
+
+console.log(typeof a);
+
+const test={
+  "name":"abc"
+}
+test.marks=20;
+test.name="xyz";
+console.log(test);
+
