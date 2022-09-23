40d6a2121acb50d282876b781e4ee9c9c97f6464
Manikanta M <59594162+manikanta3566@users.noreply.github.com>
2022-09-23 22:11:37 +0530

    Create 06_conditional_expressions.js

---
+++ 06_conditional_expressions.js
@@ -0,0 +1,36 @@
+
+//if statement
+let a = prompt("enter the value of a");
+let b = prompt("enter the value of b");;
+if (a === b) {
+  // console.log("if")
+}
+
+//if-else statement
+if (a === b) {
+  // console.log("if");
+} else {
+  //  console.log("else");
+}
+
+//if-else-if-else
+if (a == b) {
+  // console.log("if")
+} else if (a != b) {
+  // console.log("else if");
+} else {
+  //console.log("else");
+}
+
+//switch
+let fruit = "apple";
+switch (fruit) {
+  case "apple":
+    console.log("apple");
+    break;
+  case "orange":
+    console.log("orange");
+    break;
+  default:
+    console.log("default");
+}
