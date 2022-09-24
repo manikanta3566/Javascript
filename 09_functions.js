85f36e3cf5bf5590031d0c1a402b3890074eafdd
Manikanta M <59594162+manikanta3566@users.noreply.github.com>
2022-09-24 17:14:49 +0530

    Create 09_functions.js

---
+++ 09_functions.js
@@ -0,0 +1,11 @@
+//functions
+function sum(a, b) {
+  return a + b;
+}
+console.log(sum(10, 10));
+
+//arrow function
+const sumArrow = (a, b) => {
+  return a + b;
+}
+console.log(sumArrow(20, 20));
