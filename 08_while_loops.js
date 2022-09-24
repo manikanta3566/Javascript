fb3d743b4129ceb54d0937f62a77dff4c6322ba9
Manikanta M <59594162+manikanta3566@users.noreply.github.com>
2022-09-24 17:14:14 +0530

    Create 08_while_loops.js

---
+++ 08_while_loops.js
@@ -0,0 +1,15 @@
+//while loop
+let n = prompt("enter the value of n");
+n = Number.parseInt(n);
+let i = 0;
+while (i < n) {
+  console.log(i);
+  i++;
+}
+
+//do-while loop
+let j = 0;
+do {
+  console.log(j);
+  j++;
+} while (j < n)
