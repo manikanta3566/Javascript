557f21a1d2fd8af6c66b7c75dc500d9ceb7558f3
Manikanta M <59594162+manikanta3566@users.noreply.github.com>
2022-10-04 00:08:23 +0530

    Create 15_alert_prompt_confirm.js

---
+++ 15_alert_prompt_confirm.js
@@ -0,0 +1,14 @@
+//alert()
+alert("hii")
+
+//prompt()
+let a=prompt("enter the number");
+console.log(a)
+
+//confirm()
+let age=confirm("is your age is greater than 18")
+if(age){
+  console.log("your age is greater than 18")
+}else{
+  console.log("your age is less than 18")
+}
