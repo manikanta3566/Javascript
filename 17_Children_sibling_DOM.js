ef420976fef72c0a4d133c147543d47366ef852f
Manikanta M <59594162+manikanta3566@users.noreply.github.com>
2023-02-19 18:41:08 +0530

    Create 17_Children_sibling_DOM.js

---
+++ 17_Children_sibling_DOM.js
@@ -0,0 +1,6 @@
+console.log(Array.from(document.body.childNodes));
+let first=document.body.firstChild;
+console.log(first.firstChild)
+console.log(first.firstChild.nextSibling)
+console.log(first.parentNode)
+console.log(first.parentElement)
