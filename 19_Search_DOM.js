cebde985853642e363f974d20a2a1708a9cc4ece
Manikanta M <59594162+manikanta3566@users.noreply.github.com>
2023-02-21 21:25:16 +0530

    Create 19_Search_DOM.js

---
+++ 19_Search_DOM.js
@@ -0,0 +1,8 @@
+let firstElement = document.getElementsByClassName("first")
+console.log(firstElement)
+let four = document.getElementById("four")
+console.log(four)
+console.log(document.getElementsByTagName("div"));
+console.log(document.querySelector(".second"))
+console.log(document.querySelector("#four"))
+console.log(document.querySelectorAll(".first"))
