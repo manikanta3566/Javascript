c93a181582b7ad05c800fc4c976a871cee1dbbd9
Manikanta M <59594162+manikanta3566@users.noreply.github.com>
2023-03-05 15:23:45 +0530

    Create 27_event_listener.js

---
+++ 27_event_listener.js
@@ -0,0 +1,8 @@
+let btn1 = document.getElementById("btn-1");
+let btn2 = document.getElementById("btn-2");
+
+const click = () => {
+
+}
+btn1.addEventListener("click", click)
+btn2.removeEventListener("click", click)
